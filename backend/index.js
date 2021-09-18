f24745f40339e0c0a3e62e73f8cab44797be8e48
liakot khan babu <66867364+liakotkhanbabu@users.noreply.github.com>
2021-09-18 22:50:40 +0600

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,38 @@
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const typeDefs = require("./schema/typeDefs");
 const resolvers = require("./schema/resolvers");
 
 dotenv.config();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req }),
-});
-
 const PORT = process.env.PORT || 5000;
-const URI = process.env.MONGOLAB_URI;
+const URL = process.env.MONGODB_URL;
 
-mongoose
-  .connect(URI, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    return server.listen({
-      port: PORT,
-    });
-  })
-  .then((res) => {
-    console.log(`server running at ${res.url}`);
+const startServer = async () => {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req }),
   });
+
+  await server.start();
+
+  server.applyMiddleware({ app: app });
+
+  app.use((req, res) => {
+    res.send("express apollo server is running");
+  });
+
+  await mongoose.connect(URL, {
+    useNewUrlParser: true,
+  });
+  app.listen(PORT, () =>
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    )
+  );
+};
+
+startServer();
